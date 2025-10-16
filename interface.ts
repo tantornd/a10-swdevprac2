72e0a91f6e3d4eaab247f3f3e5aeb18562434583
Tantorn Dhippayom <tantornd@gmail.com>
2025-10-16 16:57:33 +0700

    A10

--- interface.ts
+++ interface.ts
@@ -1,20 +1,20 @@
-interface VenueItem {
-    _id: string,
-    name: string,
-    address: string,
-    district: string,
-    province: string,
-    postalcode: string,
-    tel: string,
-    picture: string,
-    dailyrate: number,
-    __v: number,
-    id: string
-  }
-  
-  interface VenueJson {
-    success: boolean,
-    count: number,
-    pagination: Object,
-    data: VenueItem[]
-  }
+export interface VenueItem {
+  _id: string;
+  id: string;
+  name: string;
+  address: string;
+  district: string;
+  province: string;
+  postalcode: string;
+  tel: string;
+  picture: string;
+  dailyrate: number;
+  __v: number;
+}
+
+export interface VenueJson {
+  success: boolean;
+  count: number;
+  pagination: Object;
+  data: VenueItem[];
+}
